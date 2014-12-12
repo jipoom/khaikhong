c86e66922e523db858ca7ba37c49bd85a58fa76d
jipoom <jipoom@gmail.com>
2014-12-12 08:39:56 +0700

    Khaikhong

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://10.156.13.33/khaikhong/admin/');
+define('HTTP_CATALOG', 'http://10.156.13.33/khaikhong/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://10.156.13.33/khaikhong/admin/');
+define('HTTPS_CATALOG', 'http://10.156.13.33/khaikhong/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\khaikhong/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\khaikhong/system/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\khaikhong/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\khaikhong/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\khaikhong/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\khaikhong/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\khaikhong/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\khaikhong/system/download/');
+define('DIR_UPLOAD', 'C:\xampp\htdocs\khaikhong/system/upload/');
+define('DIR_LOGS', 'C:\xampp\htdocs\khaikhong/system/logs/');
+define('DIR_MODIFICATION', 'C:\xampp\htdocs\khaikhong/system/modification/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\khaikhong/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'khaikhong');
+define('DB_PREFIX', '');
